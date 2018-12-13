523a35f5665544247aee1cf4de4f5bec078924eb
Mark <45356695+z-Phoenix-z@users.noreply.github.com>
2018-12-13 21:01:58 +0800

    Update StringCoder-FP.java

--- StringCoder-FP.java
+++ StringCoder-FP.java
@@ -5,4 +5,15 @@
      *  The returned string part has length at least 1 and largest 5. 
      *  The method gives prefernce to return the longer matches.
      */
-    private StringPart findPart(String str)
+    private StringPart findPart(String str){
+        int n = str.length();
+        int index = 0;
+        while (n != 0){
+            index = materPhrase.IndexOf(str.subtring(0,n));
+            if(index == -1){
+            n --;
+            }    
+            else {
+            return new StringPart(index,n)
+            }
+        
